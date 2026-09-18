ff6b2cc16d0c35a264f719ee866e97967b66d059
chandraprasad18 <chandraprasaddas18@gmail.com>
2026-09-18 11:45:05 +0530

    cmmit

--- src/routes/newAnnualRoutes.js
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import { getAnnualReports, updateAnnualReports } from '../controllers/newAnnualController.js';
-
-const router = express.Router();
-
-router.get('/', getAnnualReports);
-router.put('/', updateAnnualReports);
-
-export default router;
